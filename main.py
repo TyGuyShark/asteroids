65d68cc9df28478192fa5e1c15e287d31b9dd93f
tzehfus <tzehfus@gmail.com>
2025-04-12 20:32:23 +0900

    C: modified main.py to draw player, created player.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 from constants import *
+from player import *
+from circleshape import *
 
 def main():
 	pygame.init()
@@ -9,6 +11,9 @@ def main():
 	print("Starting Asteroids!")
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
+	x = SCREEN_WIDTH / 2
+	y = SCREEN_HEIGHT / 2
+	player = Player(x, y)
 
 	while True:
 		for event in pygame.event.get():
@@ -16,6 +21,7 @@ def main():
         			return
 
 		screen.fill("black")
+		player.draw(screen)
 		pygame.display.flip()
 		dt = fps_clock.tick(60) / 1000
 
