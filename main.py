7d1cbbb7094e3e7e05eca03a43124f9fea9adf74
tzehfus <tzehfus@gmail.com>
2025-04-12 19:49:10 +0900

    B: created circeshape.py, modified main.py and constants.py

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from constants import *
 
 def main():
 	pygame.init()
+	fps_clock = pygame.time.Clock()
+	dt = 0
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	print("Starting Asteroids!")
 	print(f"Screen width: {SCREEN_WIDTH}")
@@ -12,10 +14,10 @@ def main():
 		for event in pygame.event.get():
     			if event.type == pygame.QUIT:
         			return
-		black = (0, 0, 0)
-		screen.fill(black)
-		pygame.display.flip()
 
+		screen.fill("black")
+		pygame.display.flip()
+		dt = fps_clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
